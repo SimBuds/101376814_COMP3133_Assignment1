d5472cc8936347b97922921901707826bee916a7
SimBuds <casey-hsu@outlook.com>
2024-02-17 13:34:54 -0500

    Assignment 1 completed

--- resolvers.js
+++ resolvers.js
@@ -1,25 +1,29 @@
+require('dotenv').config();
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 const { UserInputError } = require('apollo-server');
 
-const User = require('../models/User');
-const Employee = require('../models/Employee');
-
-function generateToken(user) {
-  return jwt.sign(
-    {
-      id: user.id,
-      email: user.email,
-      username: user.username
-    },
-    process.env.SECRET_KEY,
-    { expiresIn: '1h' }
-  );
-}
+const User = require('./user');
+const Employee = require('./employee');
 
 module.exports = {
   Query: {
-    async getEmployees() {
+    async login(_, { username, password }) {
+      const user = await User.findOne({ $or: [{ username }, { email: username }] });
+      if (!user) {
+        throw new UserInputError('User not found');
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        throw new UserInputError('Wrong credentials');
+      }
+
+      return {
+        ...user._doc,
+        id: user._id
+      };
+    },
+    async getAllEmployees() {
       try {
         const employees = await Employee.find();
         return employees;
@@ -27,9 +31,9 @@ module.exports = {
         throw new Error(err);
       }
     },
-    async getEmployee(_, { employeeId }) {
+    async searchEmployeeById(_, { _id }) {
       try {
-        const employee = await Employee.findById(employeeId);
+        const employee = await Employee.findById(_id);
         if (employee) {
           return employee;
         } else {
@@ -41,83 +45,62 @@ module.exports = {
     },
   },
   Mutation: {
-    async register(_, { registerInput: { username, email, password } }) {
+    async signup(_, { username, email, password }) {
       const user = await User.findOne({ username });
       if (user) {
-        throw new Error('Username is taken');
+        throw new UserInputError('Username is taken');
       }
-
+    
       password = await bcrypt.hash(password, 12);
-
+    
       const newUser = new User({
         email,
         username,
         password,
         createdAt: new Date().toISOString()
       });
-
+      
       const res = await newUser.save();
-
-      const token = generateToken(res);
-
-      return {
-        ...res._doc,
-        id: res._id,
-        token
-      };
+      
+      return res._doc;
     },
-    async login(_, { username, password }) {
-      const user = await User.findOne({ username });
-      if (!user) {
-        throw new UserInputError('User not found');
-      }
-
-      const match = await bcrypt.compare(password, user.password);
-      if (!match) {
-        throw new UserInputError('Wrong credentials');
-      }
-
-      const token = generateToken(user);
-
-      return {
-        ...user._doc,
-        id: user._id,
-        token
-      };
-    },
-    async addEmployee(_, { employeeInput }) {
-      const existingEmployee = await Employee.findOne({ email: employeeInput.email });
+    async addNewEmployee(_, { first_name, last_name, email, gender, salary }) {
+      const existingEmployee = await Employee.findOne({ email });
       if (existingEmployee) {
         throw new UserInputError('Employee with this email already exists');
       }
 
-      const newEmployee = new Employee(employeeInput);
+      const newEmployee = new Employee({
+        first_name,
+        last_name,
+        email,
+        gender,
+        salary
+      });
 
       const employee = await newEmployee.save();
 
       return employee;
     },
-    async updateEmployee(_, { employeeId, updateInput }) {
-      // Find the employee to update and ensure they exist
-      const employee = await Employee.findById(employeeId);
+    async updateEmployeeById(_, { _id, first_name, last_name, email, gender, salary }) {
+      const employee = await Employee.findById(_id);
       if (!employee) {
         throw new UserInputError('Employee not found');
       }
 
-      // Check if the email is changing and if the new email is already taken
-      if (updateInput.email && updateInput.email !== employee.email) {
-        const emailExists = await Employee.findOne({ email: updateInput.email });
+      if (email && email !== employee.email) {
+        const emailExists = await Employee.findOne({ email });
         if (emailExists) {
           throw new UserInputError('Email is already in use');
         }
       }
 
-      const updatedEmployee = await Employee.findByIdAndUpdate(employeeId, updateInput, { new: true });
+      const updatedEmployee = await Employee.findByIdAndUpdate(_id, { first_name, last_name, email, gender, salary }, { new: true });
 
       return updatedEmployee;
     },
-    async deleteEmployee(_, { employeeId }) {
-      const employee = await Employee.findByIdAndDelete(employeeId);
+    async deleteEmployeeById(_, { _id }) {
+      const employee = await Employee.findByIdAndDelete(_id);
       if (!employee) {
         throw new UserInputError('Employee not found');
       }
--- server.js
+++ server.js
@@ -1,33 +1,43 @@
+require('dotenv').config();
 const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
-
-// Import your typeDefs and resolvers
-const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
+const typeDefs = require('./schema');
 
 // Create an instance of ApolloServer
 const server = new ApolloServer({
   typeDefs,
-  resolvers,
-  context: ({ req }) => ({ req })
+  resolvers
 });
-
-// Create an instance of Express
+ 
+// Create an instance of Express Server
 const app = express();
 
-// Apply the Apollo GraphQL middleware to our Express application
-server.applyMiddleware({ app });
+// Start the ApolloServer and then apply the Apollo GraphQL middleware to our Express application
+(async () => {
+  await server.start();
+  server.applyMiddleware({ app });
+})().catch(error => console.error(error));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/comp3133_assigment1', { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('MongoDB Connected');
-    return app.listen({ port: 5000 });
-  })
-  .then((res) => {
-    console.log(`Server running at ${res.url}`);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch(err => {
+  console.error(err);
+});
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong');
+});
+
+// Start the server
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
